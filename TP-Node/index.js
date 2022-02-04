184bb08a78f1f5d4c6950ccc94e0ecc6437c7e21
Yves LMDG <yves.lmdg@sfr.fr>
2022-02-04 15:32:35 +0100

    TP - Affichage du menu principal

---
+++ TP-Node/index.js
@@ -0,0 +1,7 @@
+const vue = require('./vue.js');
+const service = require('./service.js');
+
+menu = new vue.Menu("Choisir une action :");
+menu.ajouterOption("Lister les collègues");
+menu.ajouterOption("Ajouter un collègue");
+menu.afficher();
